95e8b74174ae582723a746bdbe7fd7c64fc3dd9f
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-13 00:33:02 +0100

    Fixed logic error in gradient descent

--- ml_rust/src/gradient_descent/mod.rs
+++ ml_rust/src/gradient_descent/mod.rs
@@ -20,34 +20,45 @@ use super::cost_functions;
 pub fn gradient_descent_single( x: &Vec<f32>,
                                 y: &Vec<f32>,
                                 alpha: f32,
-                                mut theta: f32) -> f32 {
+                                theta: &mut Vec<f32>)
+                                -> Box<Vec<f32>> {
     let m = y.len();
 
     let mut sum: f32;
     let mut j_theta: f32;
     let mut tmp_j_theta: f32;
+    let mut tmp_theta = theta.clone();
 
-    j_theta = cost_functions::cost_function(&x, &y, theta);
+    j_theta = cost_functions::cost_function(&x, &x, &theta);
 
     loop {
+        tmp_theta[0] = theta[0];
+        tmp_theta[1] = theta[1];
+
         sum = 0.0;
 
-        for j in 0..m {
-            sum += ((theta * x[j]) - y[j]) * x[j]
+        for i in 0..m {
+            sum += (tmp_theta[1] * x[i]) - y[i];
         }
 
-        theta = theta - alpha * sum / m as f32;
+        theta[0] = theta[0] - alpha * sum / m as f32;
+
+        sum = 0.0;
+
+        for i in 0..m {
+            sum += ((tmp_theta[1] * x[i]) - y[i]) * x[i];
+        }
+
+        theta[1] = theta[1] - alpha * sum / m as f32;
 
         tmp_j_theta = cost_functions::cost_function(&x, &y, theta);
 
         if j_theta > tmp_j_theta {
             j_theta = tmp_j_theta;
         } else {
-            break;
+            break;  // Break loop if found minium J theta
         }
-
-        println!("{}", theta);
     }
 
-    theta
+    Box::new(theta.to_vec())
 }
