63d7105179779de5400ee0bb79e3bc7975e77fbe
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-13 18:54:35 +0100

    Create cost function for multiple features.

--- ml_rust/src/cost_functions/mod.rs
+++ ml_rust/src/cost_functions/mod.rs
@@ -14,11 +14,10 @@
 /// J = sum(((theta * X[i]) - y).^2 ./(2 * m), "all");
 /// ```
 ///
-pub fn cost_function(x: &Vec<f32>,
-                        y: &Vec<f32>,
-                        theta: &Vec<f32>) -> f32{
-    let mut j_theta: f32 = 0.0;     /* The cost */
+pub fn cost_fn_single(x: &Vec<f32>, y: &Vec<f32>,
+                    theta: &Vec<f32>) -> f32 {
     let m;
+    let mut j_theta: f32 = 0.0;     /* The cost */
 
     if x.len() == y.len() {
         m = x.len();
@@ -30,8 +29,8 @@ pub fn cost_function(x: &Vec<f32>,
 
     for i in 0..m {
         j_theta += ((theta[0] + theta[1] * x[i]) - y[i])
-                        * ((theta[0] + theta[1] * x[i]) - y[i])
-                        / (2 * m) as f32;
+                    * ((theta[0] + theta[1] * x[i]) - y[i])
+                    / (2 * m) as f32;
     }
 
     j_theta
@@ -40,6 +39,7 @@ pub fn cost_function(x: &Vec<f32>,
 /// # Cost function for multiple features (x\[1\], x\[2\], ..., x\[n\]
 ///
 /// - X and y are the training sets.
+/// - X is a 2D Vector represent multiple training sets
 /// - theta is a vector that contains chosen numbers.
 ///
 /// ## Implement the following matlab formula:
@@ -48,33 +48,44 @@ pub fn cost_function(x: &Vec<f32>,
 /// J = sum(((theta[i] * X[j]) - y).^2 ./(2 * m), "all");
 /// ```
 ///
-#[allow(dead_code)]
-pub fn cost_function_multiple<'a>(x: &Vec<f32>,
-                                    y: &Vec<f32>,
-                                    theta: &Vec<f32>)
-                                    -> Box<Vec<f32>> {
+pub fn cost_fn_multiple(x: &Vec<Vec<f32>>, y: &Vec<f32>,
+                        theta: &Vec<f32>) -> Box<Vec<f32>> {
     let m;
     let n = theta.len();
+    let mut h_x: Vec<f32> = Vec::new();
 
     let mut j_theta: Vec<f32> = Vec::new();     /* The cost */
-    let mut j_theta_single: f32 = 0.0;
 
     if x.len() == y.len() {
-        m = x.len();
-    }
-    else {
-        panic!("ERROR: training sets have
-                different number of elements");
+        m = y.len();
+    } else {
+        panic!("Miss matching number of elements in training sets");
     }
 
-
-
-    for i in 0..n {
-        for j in 0..m {
-            j_theta_single += ((theta[i] * x[j]) - y[j])
-                                * ((theta[i] * x[j]) - y[j])
-                                / (2 * m) as f32;
+    for i in 0..m {
+        if x[i].len() != theta.len() {
+            panic!("Missing matching number of elements
+                    in theta and X[{}]", i);
         }
+    }
+
+    for i in 0..m {
+        for j in 0..n {
+            h_x.push(x[i][j] * theta[j]);
+        }
+    }
+
+    let mut j_theta_single: f32;
+
+    for i in 0..m {
+        j_theta_single = 0.0;
+
+        for j in 0..m {
+            j_theta_single += (h_x[j] - y[i])
+                        * (h_x[j] - y[i])
+                        / (2 * m) as f32;
+        }
+
         j_theta.push(j_theta_single);
     }
 
