57f244f95653d72745faf2eb3c72347ebbbc19da
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-14 22:51:04 +0100

    Separate data reading into separate code

--- ml_c/src/main.c
+++ ml_c/src/main.c
@@ -14,141 +14,72 @@
 #include <string.h>
 #include <malloc.h>
 
+#include "read_data.h"
 #include "cost_function.h"
 #include "gradient_descent.h"
 
 int main(int argc, char *argv[])
 {
-    FILE *fp = NULL;
 
-    char str[200];
-
-    double **X = NULL;  // features
-    double *y = NULL;   // results
-
-    int m = 0;  // number of training set
-    int n = 0;  // number of features
+    data_t *data_set = NULL;
 
     int i = 0;
     int j = 0;
 
-    /* opening file for reading */
-    fp = fopen(argv[1], "r");
+    int num_train = 0;  // number of training set
+    int num_feat = 0;   // number of features
 
-    if (fp == NULL)
-    {
-        perror("Error opening file");
-        return -1;
-    }
+    double **X = NULL;  // features
+    double *y = NULL;   // results
 
-    while(!feof(fp))
-    {
-        // Find number of training set
-        fgets(str, 200, fp);
-        m++;
-    }
+    data_set = get_data(argv[1]);   // Get data set from data file
 
-    char *token = strtok(str, ",");
-    while (token != NULL)
-    {
-        // Find number of features
-        token = strtok(NULL, ",");
-        n++;
-    }
+    X = data_set->X;
+    y = data_set->y;
+    num_train = data_set->num_train;
+    num_feat = data_set->num_feat;
 
-    printf("Number of training sets: %d\n", m);
-    printf("Number of features: %d\n", n);
-
-    rewind(fp);
-
-    #ifdef DEBUG
-        printf("Allocated memory for token\n");
-    #endif
-
-    X = calloc(m, sizeof(double));
-
-    for (i = 0; i < m; i++)
-    {
-        X[i] = calloc(n, sizeof(double));
-    }
-
-    for (i = 0; i < m; i++)
-    {
-        X[i][0] = 1.0L; // Initialised the first features into 1
-    }
-
-    y = calloc(m, sizeof(double));
-
-    i = 0;
-    while(!feof(fp))
-    {
-        fgets(str, 200, fp);    // Read line
-
-        for (j = 1; j < n; j++)
-        {
-            // Read all but the last colum into X
-            // Convert the string to double
-            X[i][j] = strtod(strtok(str, ","), NULL);
-        }
-
-        // Read the last colum into y
-        // Convert the string to double
-        y[i] = strtod(strtok(NULL, ","), NULL);
-
-        i++;    // Move to the next line
-    }
-
-    #ifdef DEBUG
-        for (i = 0; i < m; i++)
-        {
-            printf("\t%lf\t |\t%lf\n", X[i], y[i]);
-        }
-    #endif
-
-    fclose(fp);
-    fp = NULL;
-
-    #ifdef DEBUG
-        printf("Closed file\n");
-    #endif
-
-    double *theta = calloc(n, sizeof(double));
+    double *theta = calloc(num_feat, sizeof(double));
 
     printf("Thetas are [0.0, 0.0], J(theta) is %lf\n",
-                            cost_function(X, y, theta, m, n));
+                    cost_function(X, y, theta, num_train, num_feat));
 
     theta[0] = -1.0;
     theta[1] = 2.0;
 
     printf("Thetas are [-1.0, 2.0], J(theta) is %lf\n",
-                            cost_function(X, y, theta, m, n));
+                    cost_function(X, y, theta, num_train, num_feat));
 
     float alpha = 0.01;
+    int num_iters = 1500;
 
     theta[0] = 0.0;
     theta[1] = 0.0;
 
-    double *final_theta =
-                    gradient_desent(X, y, theta, alpha, m, n, 1500);
+    double *final_theta = gradient_desent(X, y, theta, alpha,
+                                            num_train, num_feat,
+                                            num_iters);
 
     printf("Found thetas using Gradient Descent: [");
 
-    for (i = 0; i < n; i++)
+    for (i = 0; i < num_feat; i++)
     {
         printf("%lf ", final_theta[i]);
     }
     printf("]\n");
 
 
-    for (i = 0; i < m; i++)
+    for (i = 0; i < num_train; i++)
     {
         free(X[i]); // Free the inner pointers before outer pointers
     }
-    free(X);
 
+    free(X);
     free(y);
     free(theta);
     free(final_theta);
+    free(data_set);
+
     #ifdef DEBUG
         printf("Freed all memory\n");
     #endif
---
+++ ml_c/src/read_data.c
@@ -0,0 +1,107 @@
+#include "read_data.h"
+
+data_t* get_data(char *file_name)
+{
+    data_t* data_set = NULL;
+
+    FILE *fp = NULL;
+
+    char str[200];
+
+    double **X = NULL;  // features
+    double *y = NULL;   // results
+
+    int m = 0;  // number of training set
+    int n = 0;  // number of features
+
+    int i = 0;
+    int j = 0;
+
+    /* opening file for reading */
+    fp = fopen(file_name, "r");
+
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    while(!feof(fp))
+    {
+        // Find number of training set
+        fgets(str, 200, fp);
+        m++;
+    }
+
+    char *token = strtok(str, ",");
+    while (token != NULL)
+    {
+        // Find number of features
+        token = strtok(NULL, ",");
+        n++;
+    }
+
+    printf("Number of training sets: %d\n", m);
+    printf("Number of features: %d\n", n);
+
+    rewind(fp);
+
+    #ifdef DEBUG
+        printf("Allocated memory for token\n");
+    #endif
+
+    X = calloc(m, sizeof(double));
+
+    for (i = 0; i < m; i++)
+    {
+        X[i] = calloc(n, sizeof(double));
+    }
+
+    for (i = 0; i < m; i++)
+    {
+        X[i][0] = 1.0L; // Initialised the first features into 1
+    }
+
+    y = calloc(m, sizeof(double));
+
+    i = 0;
+    while(!feof(fp))
+    {
+        fgets(str, 200, fp);    // Read line
+
+        for (j = 1; j < n; j++)
+        {
+            // Read all but the last colum into X
+            // Convert the string to double
+            X[i][j] = strtod(strtok(str, ","), NULL);
+        }
+
+        // Read the last colum into y
+        // Convert the string to double
+        y[i] = strtod(strtok(NULL, ","), NULL);
+
+        i++;    // Move to the next line
+    }
+
+    #ifdef DEBUG
+        for (i = 0; i < m; i++)
+        {
+            printf("\t%lf\t |\t%lf\n", X[i], y[i]);
+        }
+    #endif
+
+    fclose(fp);
+    fp = NULL;
+
+    data_set = calloc(1, sizeof(data_t));
+    data_set->X = X;
+    data_set->y = y;
+    data_set->num_train = m;
+    data_set->num_feat = n;
+
+    #ifdef DEBUG
+        printf("Closed file\n");
+    #endif
+
+    return data_set;
+}
