30202688d1e2e0849bcace0f482d9e084f346842
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-14 03:59:40 +0100

    Modified the feature normalization to use standard deviation

--- ml_rust/src/feature_scaling/mod.rs
+++ ml_rust/src/feature_scaling/mod.rs
@@ -1,8 +1,8 @@
-#[allow(dead_code)]
-pub fn zero_mean(v: &Vec<f32>) -> Box<Vec<f32>> {
+pub fn mean_normal_y(v: &Vec<f32>) -> (Box<Vec<f32>>, f32, f32) {
     let mut max: f32;
     let mut min: f32;
     let mut result: Vec<f32> = Vec::new();
+    let mut sum: f32 = 0.0;
 
     max = v[0];
     min = v[0];
@@ -16,34 +16,95 @@ pub fn zero_mean(v: &Vec<f32>) -> Box<Vec<f32>> {
         else {
             // do nothing
         }
+        sum += *i;
     }
+
+    let mean = sum / v.len() as f32;
+
+    sum = 0.0;
+    for i in v.iter() {
+        sum += (*i - mean) * (*i - mean);
+    }
+
+    let std_dev = (sum / v.len() as f32).sqrt();
 
     for i in v.iter() {
         result.push(
-            *i / (max - min)
+            (*i - mean)/ std_dev
         );
     }
 
-    Box::new(result)
+    for i in 0..v.len() {
+        println!("{}", result[i]);
+    }
+
+    (Box::new(result), mean, std_dev)
 }
 
-pub fn mean_normalization(v: &Vec<f32>) -> Box<Vec<f32>> {
-    let mut max: f32;
-    let mut result: Vec<f32> = Vec::new();
+pub fn mean_normal_x(v: &Vec<Vec<f32>>)
+            -> (Box<Vec<Vec<f32>>>, Box<Vec<f32>>, Box<Vec<f32>>) {
+    let mut max: Vec<f32> = Vec::new();
+    let mut min: Vec<f32> = Vec::new();
+    let mut mean: Vec<f32> = Vec::new();
+    let mut std_dev: Vec<f32> = Vec::new();
 
-    max = v[0];
+    let mut result: Vec<Vec<f32>> = v.clone();
 
-    for i in v.iter() {
-        if max < *i {
-            max = *i;
+    let row = v.len();
+    let col = v[0].len();
+
+    let mut sum: f32;
+
+
+    for i in v[0].iter(){
+        max.push(*i);
+        min.push(*i);
+    }
+
+    mean.push(1.0);
+    for j in 1..col {
+        sum = 0.0;
+        for i in 0..row {
+            if max[j] < v[i][j] {
+                max[j] = v[i][j];
+            } else if min[j] > v[i][j] {
+                min[j] = v[i][j];
+            } else {
+                // Do nothing
+            }
+            sum += v[i][j];
+        }
+        mean.push(sum);
+    }
+
+    for j in 1..col {
+        mean[j] = mean[j] / row as f32;
+    }
+
+    std_dev.push(1.0);
+
+    for j in 1..col {
+        sum = 0.0;
+        for i in 0..row {
+            sum += (v[i][j] - mean[j]) * (v[i][j] - mean[j]);
+        }
+
+        std_dev.push((sum / v.len() as f32).sqrt());
+    }
+
+
+    for j in 1..col {
+        for i in 0..row {
+            result[i][j] = (v[i][j] - mean[j]) / std_dev[j];
         }
     }
 
-    for i in v.iter() {
-        result.push(
-            (*i - max / 2.0) / max
-        )
+    for i in 0..row {
+        for j in 0..col {
+            print!("{}, ", result[i][j]);
+        }
+        println!("");
     }
 
-    Box::new(result)
+    (Box::new(result), Box::new(mean), Box::new(std_dev))
 }
--- ml_rust/src/main.rs
+++ ml_rust/src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     let y = *y_ptr;
 
     let alpha = 0.01;   // the learning speed
+
     let mut theta = vec![0.0, 0.0]; // set theta 0 and theta 1 to 0.0
 
     println!("Thetas are [0.0, 0.0], J(theta) is {}",
@@ -23,25 +24,41 @@ fn main() {
     println!("Thetas are [-1.0, 2.0], J(theta) is {}",
              cost_functions::get_cost(&x, &y, &vec![-1.0, 2.0]));
 
-/*     println!("Found thetas using Gradient Descent: {:?}",
+    println!("Found thetas using Gradient Descent: {:?}",
             gradient_descent::get_thetas(&x, &y,
                                         alpha, &mut theta, 1500));
- */
+
+/*
     let path = Path::new("../data_files/ex1data2.txt");
     let (x_ptr, y_ptr) = read_data::get_data(&path);
 
     let x = *x_ptr;
     let y = *y_ptr;
-    let mut ex2_theta: Vec<f32> = vec![0.0, 0.0, 0.0];
 
-    let ex_2_nor_y = feature_scaling::mean_normalization(&y);
+    let alpha = 0.01;   // the learning speed
+    let num_iters = 400;    // Number of gradient descent iterations
+    let mut theta: Vec<f32> = vec![0.0; x[0].len()];
 
-    println!("Cost (multi-featured): {}",
-    cost_functions::get_cost(&x, &ex_2_nor_y, &ex2_theta));
+    let (ex_2_nor_y, y_mean, y_std_dev)
+                    = feature_scaling::mean_normal_y(&y);
 
-    println!("Final theta for multi-featured X is {:?}",
-        gradient_descent::get_thetas(&x,
+    let (ex_2_nor_x, x_mean, x_std_dev)
+                    = feature_scaling::mean_normal_x(&x);
+
+    println!("X Mean: {:?}", x_mean);
+    println!("X Standard Deviation: {:?}", x_std_dev);
+
+    println!("Y Mean: {}", y_mean);
+    println!("Y Standard Deviation: {}", y_std_dev);
+
+    println!("Thetas are {:?}, J (the cost) is {}", theta,
+    cost_functions::get_cost(&ex_2_nor_x, &ex_2_nor_y, &theta));
+
+    println!("Found thetas using Gradient Descent: {:?}",
+        gradient_descent::get_thetas(&ex_2_nor_x,
             &ex_2_nor_y,
             alpha,
-            &mut ex2_theta, 400));
+            &mut theta, num_iters));
+ */
+
 }
