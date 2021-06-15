4ceffc8a832911045dfb8156ddb91215de628717
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-15 12:12:08 +0100

    Clarify m and n to num_train and num_feat

--- ml_rust/src/cost_functions/mod.rs
+++ ml_rust/src/cost_functions/mod.rs
@@ -16,36 +16,37 @@
 ///
 pub fn get_cost(x: &Vec<Vec<f32>>, y: &Vec<f32>,
                         theta: &Vec<f32>) -> f32 {
-    let m;
-    let n = theta.len();
+    let num_train;
+    let num_feat = theta.len();
     let mut h_x: Vec<f32> = Vec::new();
 
     let mut j_theta: f32 = 0.0;     /* The cost */
     let mut sum: f32;
 
     if x.len() == y.len() {
-        m = y.len();
+        num_train = y.len();
     } else {
         panic!("Miss matching number of elements in training sets");
     }
 
-    for i in 0..m {
+    for i in 0..num_train {
         if x[i].len() != theta.len() {
             panic!("Missing matching number of elements
                     in theta and X[{}]", i);
         }
     }
 
-    for i in 0..m {
+    for i in 0..num_train {
         sum = 0.0;
-        for j in 0..n {
+        for j in 0..num_feat {
             sum += x[i][j] * theta[j];
         }
         h_x.push(sum);
     }
 
-    for j in 0..m {
-        j_theta += (h_x[j] - y[j]) * (h_x[j] - y[j]) / (2 * m) as f32;
+    for j in 0..num_train {
+        j_theta += (h_x[j] - y[j]) * (h_x[j] - y[j])
+                    / (2 * num_train) as f32;
     }
 
     j_theta
--- ml_rust/src/gradient_descent/mod.rs
+++ ml_rust/src/gradient_descent/mod.rs
@@ -23,38 +23,37 @@ pub fn get_thetas( x: &Vec<Vec<f32>>, y: &Vec<f32>,
                         alpha: f32, theta: &mut Vec<f32>,
                         num_iters: u32) -> Box<Vec<f32>> {
 
-    let m = y.len();
-    let n = theta.len();
+    let num_train = y.len();
+    let num_feat = theta.len();
 
     let mut sum: f32;
-    let mut tmp_theta: Vec<f32> = theta.clone();
+    let mut tmp_theta: Vec<f32>;
     let mut h_x: Vec<f32> = Vec::new();
 
     for _ in 0..num_iters {
         h_x.clear();
 
-        for j in 0..n {
-            tmp_theta[j] = theta[j];
-        }
+        tmp_theta = theta.clone();
 
-        for i in 0..m {
+        for i in 0..num_train {
             sum = 0.0;
 
-            for j in 0..n {
+            for j in 0..num_feat {
                 sum += tmp_theta[j] * x[i][j];
             }
 
             h_x.push(sum);
         }
 
-        for j in 0..n {
+        for j in 0..num_feat {
             sum = 0.0;
 
-            for i in 0..m {
+            for i in 0..num_train {
                 sum += (h_x[i] - y[i]) * x[i][j];
             }
 
-            theta[j] = tmp_theta[j] - (alpha * sum / (m as f32));
+            theta[j] = tmp_theta[j]
+                        - (alpha * sum / (num_train as f32));
         }
     }
 
