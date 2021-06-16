23aa03a61cde271b13169a50c0858aa683218a35
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-16 17:47:57 +0100

    Refactor get determinant into its own function

--- ml_rust/src/linear_regression/normal_equation/mod.rs
+++ ml_rust/src/linear_regression/normal_equation/mod.rs
@@ -10,6 +10,81 @@
 /// - Slow if number of features is very large (10,000+)
 ///
 
+fn get_determinant(matrix:&Vec<Vec<f64>>) -> f64 {
+	let mut z: i32;
+	let mut determinant: f64 = 0.0;
+	let mut multiply: f64;
+	let num_feat = matrix.len();
+
+	if matrix.len() == 2 {
+		/*
+			Calculate the Determinant (der) of 2D matrix
+			M = [[A, B],
+				 [C, D]]
+			Determinant = A * D - B * C
+		*/
+
+		determinant = matrix[0][0] * matrix[1][1]
+					- matrix[0][1] * matrix[1][0];
+	} else {
+		/*
+			Calculate the Determinant (der) of larger scale matrix
+			(shortcut method)
+			M = [[A, B, C], A, B
+				[D, E, F], D, E
+				[G, H, I]] G, H;
+
+			der = A * E * I + B * F * G + C * D * H
+					- C * E * G - A * F * H - B * D * I;
+		*/
+
+		/*
+			let x: Vec<Vec<f32>> = vec![vec![0.0, 0.1, 0.2],
+										vec![1.0, 1.1, 1.2],
+										vec![2.0, 2.1, 2.2]];
+			let num_train = x.len();
+		*/
+		for i in 0..num_feat {
+			z = i as i32;	// starting column (increase each loop)
+			multiply = 1.0;
+			for j in 0..num_feat {
+
+				if z >= num_feat as i32 {
+					z = 0;	// Reset to column 0
+				}
+				// A * E * I + B * F * G + C * D * H
+				multiply *= matrix[j][z as usize];
+
+				z += 1;		// Move to next column
+			}
+			// first part of determinant
+			determinant += multiply;
+		}
+
+			for i in 0..num_feat {
+				z = (num_feat - 1 - i) as i32;
+				multiply = 1.0;
+				// starting column (decrease each loop)
+				for j in 0..num_feat {
+
+					if z < 0 {
+						z = (num_feat - 1) as i32; // Reset to column 0
+					}
+
+					// C * E * G - A * F * H - B * D * I
+					multiply *=
+					matrix[num_feat - 1 - j][z as usize];
+
+					z -= 1;		// Move to last column
+				}
+				// last part of determinant
+				determinant -= multiply;
+			}
+		}
+
+	determinant
+}
+
 pub fn get_theta(x: &Vec<Vec<f32>>, y: &Vec<f32>) -> Box<Vec<f32>> {
 
 	let mut result: Vec<f32> = Vec::new();
@@ -58,75 +133,7 @@ pub fn get_theta(x: &Vec<Vec<f32>>, y: &Vec<f32>) -> Box<Vec<f32>> {
 		mltply_rslt.push(mltply_rslt_row.clone());
 	}
 
-	let mut z: i32;
-	let mut determinant: f64 = 0.0;
-	let mut tmp_multiply: f64;
-
-	if mltply_rslt.len() == 2 {
-		/*
-			Calculate the Determinant (der) of 2D matrix
-			M = [[A, B],
-				 [C, D]]
-			Determinant = A * D - B * C
-		*/
-
-		determinant = mltply_rslt[0][0] * mltply_rslt[1][1]
-					- mltply_rslt[0][1] * mltply_rslt[1][0];
-	} else {
-		/*
-			Calculate the Determinant (der) of larger scale matrix
-			(shortcut method)
-			M = [[A, B, C], A, B
-				[D, E, F], D, E
-				[G, H, I]] G, H;
-
-			der = A * E * I + B * F * G + C * D * H
-					- C * E * G - A * F * H - B * D * I;
-		*/
-
-		/*
-			let x: Vec<Vec<f32>> = vec![vec![0.0, 0.1, 0.2],
-										vec![1.0, 1.1, 1.2],
-										vec![2.0, 2.1, 2.2]];
-			let num_train = x.len();
-		*/
-		for i in 0..num_feat {
-			z = i as i32;	// starting column (increase each loop)
-			tmp_multiply = 1.0;
-			for j in 0..num_feat {
-
-				if z >= num_feat as i32 {
-					z = 0;	// Reset to column 0
-				}
-				// A * E * I + B * F * G + C * D * H
-				tmp_multiply *= mltply_rslt[j][z as usize];
-
-				z += 1;		// Move to next column
-			}
-			// first part of determinant
-			determinant += tmp_multiply;
-		}
-
-			for i in 0..num_feat {
-				z = (num_feat - 1 - i) as i32;
-				tmp_multiply = 1.0;
-				// starting column (decrease each loop)
-				for j in 0..num_feat {
-
-					if z < 0 {
-						z = (num_feat - 1) as i32; // Reset to column 0
-					}
-
-					// C * E * G - A * F * H - B * D * I
-					tmp_multiply *=
-						mltply_rslt[num_feat - 1 - j][z as usize];
-
-					z -= 1;		// Move to last column
-				}
-				// last part of determinant
-				determinant -= tmp_multiply;
-			}
-		}
+	let determinant: f64 = get_determinant(&mltply_rslt);
 
 	/*
 		Calculate inverted X * X.transposed
@@ -143,7 +150,7 @@ pub fn get_theta(x: &Vec<Vec<f32>>, y: &Vec<f32>) -> Box<Vec<f32>> {
 		invrt_mtrx[1][0] = -mltply_rslt[1][0] / determinant;
 	} else {
 		/*
-			Support inverting large scale matrix
+			@TODO Support inverting large scale matrix
 		*/
 	}
 
