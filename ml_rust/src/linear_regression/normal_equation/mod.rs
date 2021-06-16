9e6e9f1a29025d2c4cef4657cecc449ff2fdce1f
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-16 23:25:29 +0100

    Implement matrix inversion for larger than 2x2 matrixes

--- ml_rust/src/linear_regression/normal_equation/mod.rs
+++ ml_rust/src/linear_regression/normal_equation/mod.rs
@@ -1,6 +1,5 @@
 //! # Implementation of normal equation
 //! Currently only support 2D matrix of X * X.transposed
-
 ///
 /// # Calculate the Determinant (der) of a matrix
 ///
@@ -20,6 +19,10 @@ pub fn get_determinant(matrix:&Vec<Vec<f64>>) -> f64 {
 	let mut determinant: f64 = 0.0;
 	let mut multiply: f64;
 	let num_feat = matrix.len();
+
+	if matrix.len() != matrix[0].len() {
+		panic!("Matrix not symmetrical");
+	}
 
 	if matrix.len() == 2 {
 		/*
@@ -43,12 +46,13 @@ pub fn get_determinant(matrix:&Vec<Vec<f64>>) -> f64 {
 					- C * E * G - A * F * H - B * D * I;
 		*/
 
+
 		/*
-			let x: Vec<Vec<f64>> = vec![vec![0.0, 0.1, 0.2],
-										vec![1.0, 1.1, 1.2],
-										vec![2.0, 2.1, 2.2]];
-			let num_train = x.len();
-		*/
+			let matrix: Vec<Vec<f64>> = vec![vec![0.0, 0.1, 0.2],
+											vec![1.0, 1.1, 1.2],
+											vec![2.0, 2.1, 2.2]];
+			let num_feat = matrix.len();
+ 		*/
 		for i in 0..num_feat {
 			z = i as i32;	// starting column (increase each loop)
 			multiply = 1.0;
@@ -59,33 +63,31 @@ pub fn get_determinant(matrix:&Vec<Vec<f64>>) -> f64 {
 				}
 				// A * E * I + B * F * G + C * D * H
 				multiply *= matrix[j][z as usize];
-
 				z += 1;		// Move to next column
 			}
 			// first part of determinant
 			determinant += multiply;
+
 		}
 
-			for i in 0..num_feat {
-				z = (num_feat - 1 - i) as i32;
-				multiply = 1.0;
-				// starting column (decrease each loop)
-				for j in 0..num_feat {
+		for i in 0..num_feat {
+			z = (num_feat - 1 - i) as i32;
+			multiply = 1.0;
+			// starting column (decrease each loop)
+			for j in 0..num_feat {
 
-					if z < 0 {
-						z = (num_feat - 1) as i32; // Reset to column 0
-					}
-
-					// C * E * G - A * F * H - B * D * I
-					multiply *=
-					matrix[num_feat - 1 - j][z as usize];
-
-					z -= 1;		// Move to last column
+				if z < 0 {
+					z = (num_feat - 1) as i32; // Reset to column 0
 				}
-				// last part of determinant
-				determinant -= multiply;
+
+				// C * E * G - A * F * H - B * D * I
+				multiply *= matrix[j][z as usize];
+				z -= 1;		// Move to last column
 			}
+			// last part of determinant
+			determinant -= multiply;
 		}
+	}
 
 	determinant
 }
@@ -93,16 +95,104 @@ pub fn get_determinant(matrix:&Vec<Vec<f64>>) -> f64 {
 ///# Calculate inverted matrix from provided matrix
 /// Currently using adjugate matrix
 ///```
-///	A = [[a11, a12, a13, a14],
-///		[a21, a22, a23, a24],
-///		[a31, a32, a33, a34],
-///		[a41, a42, a43, a44]];
-///```
+///	A = [[3.0, 0.0, 2.0],
+///  [2.0, 0.0, -2.0],
+///  [0.0, 1.0, 1.0]];
+///
+/// Matrix of Minors = [[0*1 - (-2)*1, 2*1 - (-2)*0, 2*1 - 0*0],
+/// 					[0*1 - 2*1, 3*1 - 2*0, 3*1 - 0*0]]
+/// A^-1 = 1/|A| * (A~)
+///
+/// ```
 ///
 pub fn get_invert(matrix: &Vec<Vec<f64>>) -> Box<Vec<Vec<f64>>> {
-	let mut result: Vec<Vec<f64>> = Vec::new();
 
-	Box::new(result)
+	let matrix = vec![vec![3.0, 0.0, 2.0],
+					vec![2.0, 0.0, -2.0],
+					vec![0.0, 1.0, 1.0]];
+
+	let mut mtrx_der: Vec<Vec<f64>> = Vec::new();
+	let mut mtrx_der_row: Vec<f64> = Vec::new();
+
+	let mut mtrx_minors: Vec<Vec<f64>> = Vec::new();
+	let mut mtrx_minors_row: Vec<f64> = Vec::new();
+
+	let mut mtrx_trans: Vec<Vec<f64>>;
+
+	let mut mtrx_result: Vec<Vec<f64>>;
+
+	let original_der: f64 = get_determinant(&matrix);
+	println!("Determinant: {}", original_der);
+
+	let row = matrix.len();
+	let col = matrix[0].len();
+
+	if row != col {
+		panic!("Matrix not symmetrical");
+	}
+
+	// Calculate matrix of minors
+	for i in 0..row {
+
+		for j in 0..col {
+
+
+			for m in 0..row {
+
+				for n in 0..col {
+					if m != i && n != j {
+						mtrx_der_row.push(matrix[m][n]);
+
+					}
+				}
+				if mtrx_der_row.len() != 0 {
+					mtrx_der.push(mtrx_der_row.clone());
+				}
+				mtrx_der_row.clear();
+
+			}
+			mtrx_minors_row.push(get_determinant(&mtrx_der));
+			mtrx_der.clear();
+		}
+		mtrx_minors.push(mtrx_minors_row.clone());
+		mtrx_minors_row.clear();
+	}
+
+	println!("{:?}", mtrx_minors);
+
+	// Matrix of Cofactors
+	for i in 0..row {
+		for j in 0..col {
+			if i % 2 == 0 && j % 2 != 0 {
+				mtrx_minors[i][j] *= -1.0;
+			} else if i % 2 != 0 && j % 2 == 0 {
+				mtrx_minors[i][j] *= -1.0;
+			}
+		}
+	}
+	println!("{:?}", mtrx_minors);
+
+	// Transpose matrix
+	mtrx_trans = mtrx_minors.clone();
+
+	for i in 0..row {
+		for j in 0..col {
+			mtrx_trans[j][i] = mtrx_minors[i][j];
+		}
+	}
+	println!("{:?}", mtrx_trans);
+
+	mtrx_result = mtrx_trans.clone();
+
+	for i in 0..row {
+		for j in 0..col {
+			mtrx_result[i][j] = mtrx_trans[i][j] / original_der;
+		}
+	}
+
+	println!("{:?}", mtrx_result);
+
+	Box::new(mtrx_result)
 }
 
 ///
@@ -162,21 +252,23 @@ pub fn get_theta(x: &Vec<Vec<f64>>, y: &Vec<f64>) -> Box<Vec<f64>> {
 		mltply_rslt.push(mltply_rslt_row.clone());
 	}
 
-	let determinant: f64 = get_determinant(&mltply_rslt);
+	// let determinant: f64 = get_determinant(&mltply_rslt);
 
 	/*
 		Calculate inverted X * X.transposed
 	*/
 	let mut invrt_mtrx = mltply_rslt.clone();
 
+	get_invert(&invrt_mtrx);
+
 	if mltply_rslt.len() == 2 {
 		/*
 			Currently only support 2x2 matrix
 		*/
-		invrt_mtrx[0][0] = mltply_rslt[1][1] / determinant;
-		invrt_mtrx[1][1] = mltply_rslt[0][0] / determinant;
-		invrt_mtrx[0][1] = -mltply_rslt[0][1] / determinant;
-		invrt_mtrx[1][0] = -mltply_rslt[1][0] / determinant;
+		// invrt_mtrx[0][0] = mltply_rslt[1][1] / determinant;
+		// invrt_mtrx[1][1] = mltply_rslt[0][0] / determinant;
+		// invrt_mtrx[0][1] = -mltply_rslt[0][1] / determinant;
+		// invrt_mtrx[1][0] = -mltply_rslt[1][0] / determinant;
 	} else {
 		/*
 			@TODO Support inverting large scale matrix
