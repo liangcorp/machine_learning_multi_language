67cb9e6cb739e53ed0ef54e9033d593c2e18fdb4
Chen Liang <chen.liang.mail@protonmail.com>
2021-06-18 03:12:32 +0100

    Create test case for get_determinant

--- ml_rust/src/lib.rs
+++ ml_rust/src/lib.rs
@@ -1,3 +1,18 @@
-pub mod linear_regression;
 pub mod feature_scaling;
+pub mod linear_regression;
 pub mod read_data;
+
+#[cfg(test)]
+
+mod tests {
+	#[test]
+	fn four_by_four(){
+		let matrix = vec![
+			vec![1.0, 1.0, 1.0, -1.0],
+			vec![1.0, 1.0, -1.0, 1.0],
+			vec![1.0, -1.0, 1.0, 1.0],
+			vec![-1.0, 1.0, 1.0, 1.0]];
+
+		assert_eq!(super::linear_regression::normal_equation::get_determinant(&matrix), -16 as f64);
+	}
+}
