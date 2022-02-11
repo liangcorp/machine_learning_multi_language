597b29064629fb2426827fdaee074e1a0d4df01b
Chen Liang <chen.liang.mail@protonmail.com>
2022-02-11 16:10:18 +0000

    Fixed the name of a declared function read_data()

--- ml_c/src/machine_learning.h
+++ ml_c/src/machine_learning.h
@@ -16,43 +16,38 @@
 #include <ctype.h>
 #include <malloc.h>
 
-typedef struct
-{
-    double **X;
-    double *y;
-    int num_train;
-    int num_feat;
+typedef struct {
+	double **X;
+	double *y;
+	int num_train;
+	int num_feat;
 } data_t;
 
-typedef struct
-{
-    double **V;
-    double *mean;
-    double *std_dev;
+typedef struct {
+	double **V;
+	double *mean;
+	double *std_dev;
 } normal_multi_t;
 
-typedef struct
-{
-    double * v;
-    double mean;
-    double std_dev;
+typedef struct {
+	double *v;
+	double mean;
+	double std_dev;
 } normal_single_t;
 
-data_t* get_data(char *file_name);
+data_t *read_data(char *file_name);
 
-double cost_function(double** X, double* y, double* theta,
-                                int no_train, int no_feat);
+double cost_function(double **X, double *y, double *theta, int no_train,
+		     int no_feat);
 
-double* gradient_desent(double **X, double *y, double *theta,
-	float alpha, int num_train, int num_feat, int num_iters);
+double *gradient_desent(double **X, double *y, double *theta, float alpha,
+			int num_train, int num_feat, int num_iters);
 
-normal_multi_t* mean_normal_multiple(double **v,
-                                    int num_train,
-                                    int num_feat);
+normal_multi_t *mean_normal_multiple(double **v, int num_train, int num_feat);
 
-normal_single_t* mean_normal_single(double *v, int num_train);
+normal_single_t *mean_normal_single(double *v, int num_train);
 
-double get_determinant(double** matrix, unsigned int m_size);
-double** get_invert(double** matrix, unsigned int m_size);
-double* normal_equation(double** X, double* y,
-                    unsigned int num_train, unsigned int num_feat);
+double get_determinant(double **matrix, unsigned int m_size);
+double **get_invert(double **matrix, unsigned int m_size);
+double *normal_equation(double **X, double *y, unsigned int num_train,
+			unsigned int num_feat);
--- ml_c/src/read_data.c
+++ ml_c/src/read_data.c
@@ -14,118 +14,109 @@
 #include <string.h>
 #include <malloc.h>
 
-typedef struct
-{
-    double **X;
-    double *y;
-    int num_train;
-    int num_feat;
+typedef struct {
+	double **X;
+	double *y;
+	int num_train;
+	int num_feat;
 } data_t;
 
 // Function name is the same of the source code file name.
 // This is for convenient purpose.
-data_t* read_data(char *file_name)
+data_t *read_data(char *file_name)
 {
-    data_t* data_set = NULL;
+	data_t *data_set = NULL;
 
-    FILE *fp = NULL;
+	FILE *fp = NULL;
 
-    char str[200];
+	char str[200];
 
-    double **X = NULL;  // features
-    double *y = NULL;   // results
+	double **X = NULL; // features
+	double *y = NULL; // results
 
-    int m = 0;  // number of training set
-    int n = 0;  // number of features
+	int m = 0; // number of training set
+	int n = 0; // number of features
 
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    /* opening file for reading */
-    fp = fopen(file_name, "r");
+	/* opening file for reading */
+	fp = fopen(file_name, "r");
 
-    if (fp == NULL)
-    {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
+	if (fp == NULL) {
+		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
 
-    while(!feof(fp))
-    {
-        // Find number of training set
-        fgets(str, 200, fp);
-        m++;
-    }
+	while (!feof(fp)) {
+		// Find number of training set
+		fgets(str, 200, fp);
+		m++;
+	}
 
-    char *token = strtok(str, ",");
-    while (token != NULL)
-    {
-        // Find number of features
-        token = strtok(NULL, ",");
-        n++;
-    }
+	char *token = strtok(str, ",");
+	while (token != NULL) {
+		// Find number of features
+		token = strtok(NULL, ",");
+		n++;
+	}
 
-    printf("Number of training sets: %d\n", m);
-    printf("Number of features: %d\n", n);
+	printf("Number of training sets: %d\n", m);
+	printf("Number of features: %d\n", n);
 
-    rewind(fp);
+	rewind(fp);
 
-    #ifdef DEBUG
-        printf("Allocated memory for token\n");
-    #endif
+#ifdef DEBUG
+	printf("Allocated memory for token\n");
+#endif
 
-    X = calloc(m, sizeof(double));
+	X = calloc(m, sizeof(double));
 
-    for (i = 0; i < m; i++)
-    {
-        X[i] = calloc(n, sizeof(double));
-    }
+	for (i = 0; i < m; i++) {
+		X[i] = calloc(n, sizeof(double));
+	}
 
-    for (i = 0; i < m; i++)
-    {
-        X[i][0] = 1.0L; // Initialized the first features into 1
-    }
+	for (i = 0; i < m; i++) {
+		X[i][0] = 1.0L; // Initialized the first features into 1
+	}
 
-    y = calloc(m, sizeof(double));
+	y = calloc(m, sizeof(double));
 
-    i = 0;
-    while(!feof(fp))
-    {
-        fgets(str, 200, fp);    // Read line
+	i = 0;
+	while (!feof(fp)) {
+		fgets(str, 200, fp); // Read line
 
-        for (j = 1; j < n; j++)
-        {
-            // Read all but the last column into X
-            // Convert the string to double
-            X[i][j] = strtod(strtok(str, ","), NULL);
-        }
+		for (j = 1; j < n; j++) {
+			// Read all but the last column into X
+			// Convert the string to double
+			X[i][j] = strtod(strtok(str, ","), NULL);
+		}
 
-        // Read the last column into y
-        // Convert the string to double
-        y[i] = strtod(strtok(NULL, ","), NULL);
+		// Read the last column into y
+		// Convert the string to double
+		y[i] = strtod(strtok(NULL, ","), NULL);
 
-        i++;    // Move to the next line
-    }
+		i++; // Move to the next line
+	}
 
-    #ifdef DEBUG
-        for (i = 0; i < m; i++)
-        {
-            printf("\t%lf\t |\t%lf\n", X[i], y[i]);
-        }
-    #endif
+#ifdef DEBUG
+	for (i = 0; i < m; i++) {
+		printf("\t%lf\t |\t%lf\n", X[i], y[i]);
+	}
+#endif
 
-    fclose(fp);
-    fp = NULL;
+	fclose(fp);
+	fp = NULL;
 
-    data_set = calloc(1, sizeof(data_t));
-    data_set->X = X;
-    data_set->y = y;
-    data_set->num_train = m;
-    data_set->num_feat = n;
+	data_set = calloc(1, sizeof(data_t));
+	data_set->X = X;
+	data_set->y = y;
+	data_set->num_train = m;
+	data_set->num_feat = n;
 
-    #ifdef DEBUG
-        printf("Closed file\n");
-    #endif
+#ifdef DEBUG
+	printf("Closed file\n");
+#endif
 
-    return data_set;
+	return data_set;
 }
